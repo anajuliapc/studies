cb50a1c7f9ba6d4414da3820eb447d69bdedd879
anajuliapc <anajuliapc@gmail.com>
2021-08-20 11:41:00 -0300

    Solve runner up score problem

--- python/hackerrank/runner_up_score.py
+++ python/hackerrank/runner_up_score.py
@@ -4,3 +4,7 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
+
+    arrset = set(arr) # turn into set to remove duplicates
+    arrset.remove(max(arrset)) # remove max value
+    print(max(arrset)) # next max value is the runner up score
